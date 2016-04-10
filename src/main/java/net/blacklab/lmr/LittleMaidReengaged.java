a23bd68eb22c2ae799646518dda843a2fc97771b
Verclene <xdev58bo@gmail.com>
2016-04-10 22:40:09 +0900

    Modify versions

--- src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
+++ src/main/java/net/blacklab/lmr/LittleMaidReengaged.java
@@ -14,12 +14,18 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = LittleMaidReengaged.DOMAIN, name = LittleMaidReengaged.NAME, version = LittleMaidReengaged.VERSION, dependencies = LittleMaidReengaged.DEPENDENCIES)
+@Mod(
+		modid = LittleMaidReengaged.DOMAIN,
+		name = LittleMaidReengaged.NAME,
+		version = LittleMaidReengaged.VERSION,
+		dependencies = LittleMaidReengaged.DEPENDENCIES,
+		acceptedMinecraftVersions = LittleMaidReengaged.ACCEPTED_MCVERSIONS)
 public class LittleMaidReengaged {
 	public static final String DOMAIN = "lmreengaged";
 	public static final String NAME = "LittleMaid Porter NXtoRE";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.8";
 	public static final String DEPENDENCIES = "required-after:lmmx@[5.2.84,)";
+	public static final String ACCEPTED_MCVERSIONS = "[1.8,1.8.9]";
 
 	public static Item maidPorter;
 
