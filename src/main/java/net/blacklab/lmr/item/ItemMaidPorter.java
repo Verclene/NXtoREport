9689d88b4cd67458338e6f0ea03f4329254820d4
Verclene <xdev58bo@gmail.com>
2016-04-10 21:02:22 +0900

    1.0.7: Add localize & tooltip

--- src/main/java/net/blacklab/lmr/item/ItemMaidPorter.java
+++ src/main/java/net/blacklab/lmr/item/ItemMaidPorter.java
@@ -1,7 +1,13 @@
 package net.blacklab.lmr.item;
 
+import java.util.List;
+
+import net.blacklab.lmmnx.entity.littlemaid.exp.ExperienceUtil;
 import net.blacklab.lmr.LittleMaidReengaged;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemMaidPorter extends Item {
 	public ItemMaidPorter() {
@@ -12,5 +18,19 @@ public class ItemMaidPorter extends Item {
 	@Override
 	public boolean isDamageable() {
 		return true;
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List tooltip, boolean advanced) {
+		NBTTagCompound stackTag = stack.getTagCompound();
+		if (stackTag != null) {
+			String customName = stackTag.getString(LittleMaidReengaged.DOMAIN + ":MAID_NAME");
+			float experience = stackTag.getFloat(LittleMaidReengaged.DOMAIN + ":EXPERIENCE");
+			
+			if (!customName.isEmpty()) {
+				tooltip.add("Name: ".concat(customName));
+			}
+			tooltip.add(String.format("Level: %3d", ExperienceUtil.getLevelFromExp(experience)));
+		}
 	}
 }
